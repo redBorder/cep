cfefbe5a602461895adaa76c2d437056739e2b36
jmf <jaime.mf.92@gmail.com>
2015-06-24 11:06:20 +0200

    Full API

--- src/main/java/net/redborder/correlation/rest/RESTManager.java
+++ src/main/java/net/redborder/correlation/rest/RESTManager.java
@@ -1,7 +1,52 @@
 package net.redborder.correlation.rest;
 
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
 /**
  * Created by jmf on 11/06/15.
  */
 public class RESTManager {
+
+    // Base URI the Grizzly HTTP server will listen on
+    private String BASE_URI = "http://localhost:8080/myapp/";
+    private ResourceConfig rc;
+    private HttpServer server;
+
+
+    /*Realizar constructor que establezca la configuracion:
+    *       -URI:ip/port/
+    *
+    * */
+    public RESTManager(){
+        // create a resource config that scans for JAX-RS resources and providers
+        // in com.example package
+        rc = new ResourceConfig().packages("net.redborder.correlation.rest");
+
+    }
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public void startServer() {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in com.example package
+        //rc = new ResourceConfig().packages("net.redborder.correlation.rest");
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public void stopServer(){
+
+        server.stop();
+    }
+
 }
--- src/main/java/net/redborder/correlation/rest/Resource.java
+++ src/main/java/net/redborder/correlation/rest/Resource.java
@@ -1,7 +1,69 @@
 package net.redborder.correlation.rest;
 
+import javax.inject.Singleton;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  * Created by jmf on 24/06/15.
  */
+
+@Singleton
+@Path("res")
 public class Resource {
+
+    private Queries queries = new Queries();
+
+    /**
+     *
+     * Método que manejará las peticiones HTTP POST con formato JSON.
+     * Mediante este método se incluirán las querys enviadas por el usuario.
+     *
+     * @param query Acepta una cadena en formato JSON que contiene la query
+     *
+     *
+     * @return Respuesta con el código obtenido al tratar la petición
+     */
+
+    @POST
+    @Path("send")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response receiveQuery(String query){
+
+        //Comprobamos que la petición de inclusión ha sido éxitosa
+        if(queries.addQuery(query))
+            return Response.status(200).entity(query).build();
+        else
+            return Response.status(202).entity(query).build();
+
+    }
+
+    /**
+     * Método que manejará las peticiones HTTP DELETE.
+     * Mediante este método se eliminarán las querys enviadas por el usuario.
+     *
+     *
+     * @param id Se incluirá en la petición el id de la query a eliminar
+     *
+     * @return Respuesta con el código obtenido al tratar la petición
+     */
+
+    @DELETE
+    @Path("/delete/{id}")
+    @Consumes (MediaType.APPLICATION_JSON)
+    public Response deleteQuery(@PathParam("id") int id){
+
+        boolean isDelete = false;
+
+        isDelete = queries.deleteQuery(id);
+
+        //Comprobamos que la petición de borrado ha sido éxitosa
+        if(isDelete)
+            return Response.status(200).build();
+        else
+            return Response.status(404).entity("Query with the id " + id + " is not present ").build();
+
+
+    }
 }
