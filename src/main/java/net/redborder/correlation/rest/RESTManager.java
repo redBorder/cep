46eb7d1f98d479763f13e8454adfaf2958f91c50
jmf <jaime.mf.92@gmail.com>
2015-06-24 11:28:58 +0200

    Add method to stop http server and RESTManager.java logging

--- src/main/java/net/redborder/correlation/rest/RESTManager.java
+++ src/main/java/net/redborder/correlation/rest/RESTManager.java
@@ -4,6 +4,8 @@ package net.redborder.correlation.rest;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -12,6 +14,8 @@ import java.net.URI;
  * Created by jmf on 11/06/15.
  */
 public class RESTManager {
+
+    private final Logger log = LoggerFactory.getLogger(RESTManager.class);
 
     // Base URI the Grizzly HTTP server will listen on
     private String BASE_URI = "http://localhost:8080/myapp/";
@@ -42,11 +46,16 @@ public class RESTManager {
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
         server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+
+        if(server != null)
+            log.info("Starting server...");
+        else
+            log.error("Server is not initialized");
     }
 
     public void stopServer(){
-
-        server.stop();
+        if(server != null)
+            server.shutdown();
     }
 
 }
